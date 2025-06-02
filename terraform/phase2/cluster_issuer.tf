59a9a82ab01b6edde7132024523890013652a5a9
romskor <romskor11@gmail.com>
2025-06-02 08:42:49 +0000

    edit cluster issuer

--- terraform/phase2/cluster_issuer.tf
+++ terraform/phase2/cluster_issuer.tf
@@ -1,11 +1,27 @@
+resource "null_resource" "wait_for_clusterissuer_crd" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for ClusterIssuer CRD to be registered..."
+      for i in {1..10}; do
+        if kubectl get crd clusterissuers.cert-manager.io > /dev/null 2>&1; then
+          echo " ClusterIssuer CRD is ready."
+          break
+        fi
+        echo "Still waiting... ($i)"
+        sleep 5
+      done
+    EOT
+  }
+  depends_on = [helm_release.cert_manager]
+}
+
 resource "kubernetes_manifest" "cluster_issuer" {
   manifest = yamldecode(templatefile("${path.module}/cluster-issuer.yaml", {
     email = var.email
   }))
 
   depends_on = [
-    helm_release.cert_manager,
-    kubernetes_namespace.cert_manager
+    null_resource.wait_for_clusterissuer_crd
   ]
 }
 
